d8892a22847875867c6aead95866ccaaf004fd39
Rebeca Costa Frutuoso <108592549+rebecafrutuoso@users.noreply.github.com>
2022-11-04 23:19:10 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Componentes/header";
+import body from "./Componentes/body";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <body />
+    </>
+  );
+}
